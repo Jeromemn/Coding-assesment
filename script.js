fc2de5d4161f3415462c9d7b413df9bb15242fa2
nixon <jeromenixon95@gmail.com>
2022-09-19 23:37:54 -0700

    added functions, subtracted time for incorrect answers, created start event, display questions function triggers when start is selected, displays score not yet getting score added from correct answers,added event listener for answer options

--- script.js
+++ script.js
@@ -1,39 +1,156 @@
-//var body = document.body;
-//var h1 = document.createElement("h1");
-//var h1 = document.querySelector("h1");
-//h1.textContent = "Coding Assesment";
-//var h2 =document.createElement("h2");
-var timeEl = document.querySelector(".time");
+const timeEl = document.querySelector(".time");
+const timerEl = document.getElementById('countdown');
+const startButton = document.getElementById('start');
+const mainEl = document.getElementById("main");
+const mainHeader = document.getElementsByClassName(".main header");
+const question = document.getElementById("question");
+const choiceA = document.getElementById("a");
+const choiceB = document.getElementById("b");
+const choiceC = document.getElementById("c");
+const choiceD = document.getElementById("d");
+const scoreContainer = document.getElementById("score-Container");
+const questionContainer = document.getElementById("question-container");
+const scoreTotal = document.getElementById("score");
 
-var mainEl = document.getElementById("main");
+let currentQuestionIndex = 0
+
+let timeLeft = 10;
+
+let score = 0;
 
 
-//var questions = document.querySelector(h2);
 
-//const button =document.querySelector("button")
-//button.setAttribute("name", Start)
-//button.setAttribute("style", "font-size:20px")
-//var h2. textContent = "question";
+const questions = [
+    {
+        question: "question 1",
+        choices: ["a", "b", "c", "d"],
+        answer: "a",
+    },
+    {
+        question: "question 2",
+        choices: ["a", "b", "c", "d"],
+        answer: "a",
+    },
+    {
+        question: "question 3",
+        choices: ["a", "b", "c", "d"],
+        answer: "a",
+    },
+    {
+        question: "question 4",
+        choices: ["a", "b", "c", "d"],
+        answer: "a",
+    },
+    {
+        question: "question 5",
+        choices: ["a", "b", "c", "d"],
+        answer: "a",
+    },
+];
 
-var secondsLeft = 10;
+const answers = []
 
-function setTime() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft + " Seconds till Game Over";
+const points = []
 
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
+function start(event) {
+    // reset global variables
+    // show start button and hide end screen
+    startButton.style.display = 'none';
+
+    displayQuestion(currentQuestionIndex);
+    countdown();
+    displayScore()
+}
+
+startButton.addEventListener("click", start);
+
+
+function countdown() {
+
+
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + 'second remaining';
+            timeLeft--;
+        } else if (timeLeft === 0) {
+            timerEl.textContent = 0 + ' seconds remaining';
+            timeLeft--;
+            var imgEl = document.createElement("img");
+            imgEl.setAttribute("src", "gameover-text.jpg");
+            mainEl.appendChild(imgEl);
+            questionContainer.style.display = 'none';
+            // if time is 0 then show score 
+            // sutract time when answer is wrong 
         }
     }, 1000);
 }
+function displayQuestion(index) {
 
-function sendMessage() {
-    timeEl.textContent = " ";
-    var imgEl = document.createElement("img");
-    imgEl.setAttribute("src", "Game-over.jpg");
-    mainEl.appendChild(imgEl);
+    question.textContent = questions[index].question;
+    choiceA.textContent = questions[index].choices[0];
+    choiceB.textContent = questions[index].choices[1];
+    choiceC.textContent = questions[index].choices[2];
+    choiceD.textContent = questions[index].choices[3];
+    questionContainer.style.display = 'block';
+
+    choiceA.addEventListener("click", selectAnswer)
+    choiceB.addEventListener("click", selectAnswer)
+    choiceC.addEventListener("click", selectAnswer)
+    choiceD.addEventListener("click", selectAnswer)
+
 }
 
-setTime();
+function selectAnswer(event) {
+    console.log(event.target.textContent);
+    answers.push(event.target.textContent);
+    if (!checkAnswer(currentQuestionIndex)) {
+        timeLeft -= 5;
+    }
+    currentQuestionIndex += 1;
+    console.log(currentQuestionIndex);
+
+    if (currentQuestionIndex === questions.length) {
+        // if on last question and answered in time, display end screen 
+        questionContainer.style.display = 'none';
+        console.log(answers)
+        return;
+    }
+
+    displayQuestion(currentQuestionIndex);
+
+
+}
+
+
+function checkAnswer(index) {
+    if (questions[index].answer === answers[index]) {
+        return true;
+    }
+    return false;
+}
+function displayScore() {
+        scoreContainer.textContent = score;
+    scoreContainer.style.display = "block";
+
+    Total();
+}
+function Total() {
+    if (checkAnswer(currentQuestionIndex)) {
+        score += 1;
+    }
+
+}
+
+
+
+
+
+
+// make try again button
+// local storage to save score
+////show high score lisy
+
+// functions displayEnd()
